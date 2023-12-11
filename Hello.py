80c750445283e5aaa8623f8c807e623995b92e2a
Christian <elchristog@gmail.com>
2023-12-11 14:26:59 +0000

    feat: tool descriptions at top

--- Hello.py
+++ Hello.py
@@ -26,23 +26,9 @@ def run():
 
     st.write("# A+R Store Product Information Scraper! 🛋️")
 
-    # st.sidebar.success("Select a demo above.")
-
     st.markdown(
         """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
+        To utilize this tool, kindly click the "Scrape" button. Please be patient for a few seconds, and subsequently, you will be able to download the CSV file containing the scraped data. Additionally, the tool will include general statistics regarding the collected information about the products in the A+R Store.
     """
     )
 
